c2618926938165d5dcaa9e06d2a990aee51e4167
CLX4399 <whtclx@outlook.com>
2020-10-26 10:29:11 +0800

    renfast 后台管理版本倒是nacos无法获取正确ip地址

--- gulimall-product/src/test/java/com/clx4399/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/clx4399/gulimall/product/GulimallProductApplicationTests.java
@@ -1,14 +1,18 @@
 package com.clx4399.gulimall.product;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.clx4399.gulimall.product.entity.BrandEntity;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.GetObjectRequest;
 import com.clx4399.gulimall.product.service.BrandService;
 import com.clx4399.gulimall.product.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
@@ -18,6 +22,17 @@ class GulimallProductApplicationTests {
 
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Test
+    void  testUplaod() throws FileNotFoundException {
+
+        InputStream inputStream = new FileInputStream("C:\\Users\\WhtCl\\Downloads\\三体智子高清4k动漫壁纸_彼岸图网.jpg");
+            // download file to local
+            ossClient.putObject("gulimal-clx4399", "三体智子高清4k动漫壁纸_彼岸图网.jpg", new File("C:\\Users\\WhtCl\\Downloads\\三体智子高清4k动漫壁纸_彼岸图网.jpg"));
+    }
 
     @Test
     void contextLoads() {
