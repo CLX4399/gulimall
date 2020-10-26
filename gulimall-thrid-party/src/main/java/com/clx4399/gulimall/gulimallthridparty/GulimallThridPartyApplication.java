04921be85679521da26c4e07cb754b4c288ac136
CLX4399 <whtclx@outlook.com>
2020-10-27 00:42:26 +0800

    renfast 添加第三方组件

---
+++ gulimall-thrid-party/src/main/java/com/clx4399/gulimall/gulimallthridparty/GulimallThridPartyApplication.java
@@ -0,0 +1,13 @@
+package com.clx4399.gulimall.gulimallthridparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallThridPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallThridPartyApplication.class, args);
+    }
+
+}
---
+++ gulimall-thrid-party/src/test/java/com/clx4399/gulimall/gulimallthridparty/GulimallThridPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clx4399.gulimall.gulimallthridparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallThridPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
