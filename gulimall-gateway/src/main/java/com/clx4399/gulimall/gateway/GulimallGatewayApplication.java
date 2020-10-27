0d23635ee63e67f59fb3c9a131ac3ea0cce98bce
CLX4399 <whtclx@outlook.com>
2020-10-27 11:27:14 +0800

    renfast 产品服务组件，nacos设置外部配置

--- gulimall-gateway/src/main/java/com/clx4399/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/clx4399/gulimall/gateway/GulimallGatewayApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallGatewayApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(GulimallGatewayApplication.class, args);
     }
 
