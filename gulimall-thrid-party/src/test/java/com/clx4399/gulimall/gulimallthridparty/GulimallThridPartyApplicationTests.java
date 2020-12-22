9ea80f1193bd5ee80916937c3576b1a6dad3092a
CLX4399 <whtclx@outlook.com>
2020-12-22 16:48:36 +0800

    elasticsearch功能模块新增->测试调试

--- gulimall-thrid-party/src/test/java/com/clx4399/gulimall/gulimallthridparty/GulimallThridPartyApplicationTests.java
+++ gulimall-thrid-party/src/test/java/com/clx4399/gulimall/gulimallthridparty/GulimallThridPartyApplicationTests.java
@@ -1,20 +1,12 @@
 package com.clx4399.gulimall.gulimallthridparty;
 
-import com.aliyun.oss.OSSClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @SpringBootTest
 class GulimallThridPartyApplicationTests {
-
-    @Autowired
-    private OSSClient ossClient;
 
     @Test
     void  testUplaod() throws FileNotFoundException {
