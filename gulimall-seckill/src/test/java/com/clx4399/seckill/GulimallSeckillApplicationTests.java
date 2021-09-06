e6158efd144d4c34d04e36b223d8d0096c437947
CLX4399 <whtclx@outlook.com>
2021-09-06 10:52:47 +0800

    秒杀模块 -> 搭建

---
+++ gulimall-seckill/src/test/java/com/clx4399/seckill/GulimallSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clx4399.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
